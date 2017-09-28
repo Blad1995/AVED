dcc168bff777ea94fc6c1427cadebbe6f99d5d3a
Ondřej Divina <x451338@fi.muni.cz>
2017-09-28 14:03:12 +0200

    hw02 hotovo

--- hw02/hw02.R
+++ hw02/hw02.R
@@ -10,22 +10,22 @@ rm(list = ls())
 load("hw02.RData")
 
 # 1. count how many subjects you have and save it
-number_of_subjects <-
+number_of_subjects <- length(person)
 save(number_of_subjects, file = "results.RData")
 
 # 2. calculate median weight and save it
-med_weight <-
+med_weight <- median(weight)
 save(number_of_subjects, med_weight, file = "results.RData")
 
 # 3. for each person, calculate BMI and round it to tenths
-bmi <-
+bmi <- round(weight / (height / 100) ^ 2, digits = 1)
 save(number_of_subjects, med_weight, bmi, file = "results.RData")
 
 # 4. count obese subjects
-number_of_obese <-
+number_of_obese <- sum(bmi >= 30)
 save(number_of_subjects, med_weight, bmi, number_of_obese, file = "results.RData")
 
 # 5. create a vector with the names of the obese subjects
-obese_subjects <-
+obese_subjects <- person[bmi >= 30]
 save(number_of_subjects, med_weight, bmi, number_of_obese, obese_subjects,
      file = "results.RData")
