7dc4cd8b7511492e29d85f2e6ed42d906473565f
Ondřej Divina <Ondra.Divina@seznam.cz>
2017-10-04 12:55:35 +0200

    hw03 kromě posledního úkolu

--- hw03/hw03.R
+++ hw03/hw03.R
@@ -14,16 +14,28 @@ load("hw03.RData")
 
 # 1. in dataset modal_split: keep only rows where vehicle is TRN and the data are not
 #    older than 2004-01-01; remove the rest
+
+modal_split <- modal_split[modal_split$vehicle == "TRN" & modal_split$time >= "2004-01-01",]
 save(modal_split, file = "results.RData")
 
 # 2. in dataset train_total: keep only rows where units are MIO_PKM; remove the rest
+train_total <-  train_total[train_total$unit == "MIO_PKM", ]
 save(modal_split, train_total, file = "results.RData")
 
 # 3. create new (tibble) dataset transit with the following variables: time, train_share,
 #    train_volume, and transit_volume
+
+transit <- train_total["time"]
+transit$train_share <- modal_split$values
+transit$train_volume <- train_total$values
+transit$transit_volume <-  transit$train_volume * 100 / transit$train_share
+
 save(modal_split, train_total, transit, file = "results.RData")
 
 # 4. sort transit dataset so that the first row is for 2004, the second for 2005, etc.
+
+transit <- transit[order(transit$time, decreasing = FALSE), ]
+
 save(modal_split, train_total, transit, file = "results.RData")
 
 # 5.  add transit_growth variable to the dataset transit
